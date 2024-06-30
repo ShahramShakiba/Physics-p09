050417cebd216527626d2fa2b68a91a3b4290241
ShahramShakiba <shahramshakiba88@gmail.com>
2024-06-30 16:34:51 +0330

    Handle Multiple Objects by creating a Function

--- src/script.js
+++ src/script.js
@@ -41,22 +41,6 @@ const defaultContactMaterial = new Cannon.ContactMaterial(
 world.addContactMaterial(defaultContactMaterial);
 world.defaultContactMaterial = defaultContactMaterial;
 
-//============ Sphere
-const sphereShape = new Cannon.Sphere(0.5);
-const sphereBody = new Cannon.Body({
-  mass: 1,
-  position: new Cannon.Vec3(0, 4, 0),
-  shape: sphereShape,
-  // material: defaultMaterial,
-});
-
-sphereBody.applyLocalForce(
-  new Cannon.Vec3(100, 0, 0), // force
-  new Cannon.Vec3(0, 0, 0) // localPoint
-);
-
-world.addBody(sphereBody);
-
 //=========== Floor
 const floorShape = new Cannon.Plane();
 const floorBody = new Cannon.Body();
@@ -68,20 +52,6 @@ floorBody.quaternion.setFromAxisAngle(new Cannon.Vec3(-1, 0, 0), Math.PI / 2);
 // floorBody.position; // it's in the center of the scene, we don't want to move it - don't touch the position
 
 world.addBody(floorBody);
-
-/* Test sphere */
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 32, 32),
-  new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
-  })
-);
-sphere.castShadow = true;
-sphere.position.y = 0.5;
-scene.add(sphere);
 
 //================= Floor =========================
 const floor = new THREE.Mesh(
@@ -144,6 +114,41 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+//=================== Utils ========================
+const objects = [];
+
+const createSphere = (radius, position) => {
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(radius, 20, 20),
+    new THREE.MeshStandardMaterial({
+      metalness: 0.3,
+      roughness: 0.4,
+      envMap: environmentMapTexture,
+    })
+  );
+
+  sphere.castShadow = true;
+  sphere.position.copy(position);
+  scene.add(sphere);
+
+  //======== Cannon.js Body
+  const shape = new Cannon.Sphere(radius);
+  const body = new Cannon.Body({
+    mass: 1,
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  objects.push({
+    sphere,
+    body,
+  });
+};
+
+createSphere(0.5, { x: 0, y: 4, z: 0 });
+
 //=================== Animate ========================
 const clock = new THREE.Clock();
 let prevTime = 0;
@@ -153,13 +158,12 @@ const tick = () => {
   const deltaTime = elapsedTime - prevTime;
   prevTime = elapsedTime;
 
-  //===== Update Physics World           force            worldPoint
-  sphereBody.applyForce(new Cannon.Vec3(-0.2, 0, 0), sphereBody.position);
-
+  //===== Update Physics World
   world.step(1 / 60, deltaTime, 3); // 01
 
-  // Update positions based-on physics world - 02
-  sphere.position.copy(sphereBody.position);
+  for (const obj of objects) {
+    obj.sphere.position.copy(obj.body.position);
+  }
 
   controls.update();
   renderer.render(scene, camera);
@@ -177,4 +181,45 @@ tick();
     3. how much iterations the world can apply to catch up with a 
     potential delay,  // 3 - three steps
      )
+*/
+
+/* Test
+///============ Sphere
+const sphereShape = new Cannon.Sphere(0.5);
+const sphereBody = new Cannon.Body({
+  mass: 1,
+  position: new Cannon.Vec3(0, 4, 0),
+  shape: sphereShape,
+  /// material: defaultMaterial,
+});
+
+sphereBody.applyLocalForce(
+  new Cannon.Vec3(100, 0, 0), // force
+  new Cannon.Vec3(0, 0, 0) // localPoint - center of the ball
+);
+
+world.addBody(sphereBody);
+
+
+/// Test sphere 
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5,
+  })
+);
+sphere.castShadow = true;
+sphere.position.y = 0.5;
+scene.add(sphere);
+
+
+  ///===== Update Physics World        03- force          worldPoint
+  sphereBody.applyForce(new Cannon.Vec3(-0.2, 0, 0), sphereBody.position);
+
+  
+  /// Update positions based-on physics world - 02
+  sphere.position.copy(sphereBody.position);
 */
